4451631a2ef745c61b2abbc8c5302467335f7d6e
achal <achalgajbhiye19@gmail.com>
2024-08-14 23:46:00 +0530

    Add

---
+++ Day3/Basic.java
@@ -0,0 +1,21 @@
+public class Basic{
+
+    public static void main(String[] args){
+
+        System.out.println("Hello java");
+
+    }
+}
+
+/*
+ *  1. Classname should be in CamelCase
+ *  2. public static void main meaning
+ *     public - access modifier
+ *     static - accesss modifier which allow main function to execute without creating object
+ *     void - return type which does not return anything
+ *     main - function
+ *  3. System.out.println meaning 
+ *     System - class in java
+ *     out - static variable in System class of type printStream
+ *     println - method present in printStream class
+ */
